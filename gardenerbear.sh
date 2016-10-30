d19691d655438efec4187e92cc215601c2328721
bromptonista <bromptonista@yahoo.com>
2016-10-30 23:34:49 +0000

    update gardenerbear.sh
    Auto-update script from GitHub

--- gardenerbear.sh
+++ gardenerbear.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+# setup cronjob eg crontab -e and add: @hourly gardenerbear.sh
 if ps -ef | grep -v grep | grep gardenerbear_full.py ; then
         exit 0
 else
+        wget -N https://raw.githubusercontent.com/bromptonista/gardenerbear/master/gardenerbear_full.py
         sudo python gardenerbear_full.py &
         #Write note to Logfile
         echo "[`date`]: gardenerbear_full.py was not running... Restarted" >> gardenerbear.log
